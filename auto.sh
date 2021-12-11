bd3d11d9e4ea5608c106a3bce6d8ae813cf5d3b7
sfab07 <sfab07@gmail.com>
2021-12-11 17:22:08 +0100

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,9 +1,9 @@
-sudo apt install apt-transport-https curl
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser
+sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 
-https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb  && sudo dpkg -i https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get update
 
-sudo apt-get install -y spacefm deadbeef zenity radiotray inkscape cherrytree webp gthumb wine playonlinux
+sudo apt-get install -y brave-browser google-chrome-stable deadbeef inkscape cherrytree spacefm webp nautilus lollypop radiotray-lite jnettop pdftk python3-pip eyed3 mencoder 
+
+# autres : zuluscrypt, xmind, yed, refind
